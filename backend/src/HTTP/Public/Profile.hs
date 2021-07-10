b3f7ec044c1751c7899cd3d8dd85aff3f342fbd8
fanshi1028 <jackychany321@gmail.com>
2021-07-10 17:51:53 +0800

    little refactor

--- backend/src/HTTP/Public/Profile.hs
+++ backend/src/HTTP/Public/Profile.hs
@@ -11,9 +11,9 @@ import Domain.User (UserR)
 import Domain.Util.JSON.To (Out (Out))
 import HTTP.Util (ReadApi)
 import Servant (Capture, ServerT, type (:>))
-import qualified VisitorAction (E (GetProfile))
+import VisitorAction (E (GetProfile))
 
 type ProfileApi = Capture "username" (UserR "id") :> ReadApi UserR "profile"
 
 profileServer :: (Member VisitorAction.E sig, Algebra sig m) => ServerT ProfileApi m
-profileServer uid = Out <$> send (VisitorAction.GetProfile uid)
+profileServer = Out <<$>> send . GetProfile
