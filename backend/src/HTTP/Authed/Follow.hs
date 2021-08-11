d3870b424c3e96ef56f2334498781017251ac566
fanshi <jackychany321@gmail.com>
2021-08-12 00:39:38 +0800

    implement follow api

--- backend/src/HTTP/Authed/Follow.hs
+++ backend/src/HTTP/Authed/Follow.hs
@@ -1,15 +1,23 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
 
 -- |
 module HTTP.Authed.Follow (FollowApi, followServer) where
 
-import Domain.User (UserR)
+import Control.Algebra (Algebra, send)
+import Control.Effect.Sum (Member)
+import Domain.User (UserR (..))
+import Domain.Util.JSON.To (Out (Out))
 import HTTP.Util (ToggleApi)
-import Servant (Capture, ServerT, type (:>))
+import Servant (Capture, ServerT, type (:<|>) ((:<|>)), type (:>))
+import qualified UserAction
 
 type FollowApi = Capture "username" (UserR "id") :> "follow" :> ToggleApi UserR "profile"
 
--- FIXME
-followServer :: ServerT FollowApi m
-followServer = undefined
+followServer ::
+  ( Algebra sig m,
+    Member UserAction.E sig
+  ) =>
+  ServerT FollowApi m
+followServer uid =
+  (Out <$> send (UserAction.FollowUser uid))
+    :<|> (Out <$> send (UserAction.UnfollowUser uid))
