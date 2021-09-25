cc9983415fe271595f89a05a2b4be0924430fd99
fanshi1028 <jackychany321@gmail.com>
2021-09-25 22:17:08 +0800

    remove some shrinking instances

--- test/Gen/Realistic.hs
+++ test/Gen/Realistic.hs
@@ -109,14 +109,12 @@ instance Arbitrary (Realistic Username) where
           Funny.name,
           Name.name
         ]
-  shrink (Realistic (Username u)) = Realistic . Username <$> shrink u
 
 -- | @since 0.2.0.0
 -- >>> sample' $ arbitrary @(Realistic Bio)
 -- ["See how she leans her cheek upon her hand. Oh, that I were a glove upon that hand that I might touch that cheek!","When you play a game of thrones you win or you die.","The world is grown so bad that wrens make prey where eagles dare not perch.","The world is grown so bad that wrens make prey where eagles dare not perch.","You know what they say: You can't teach a gay dog straight tricks","The world is grown so bad that wrens make prey where eagles dare not perch.","Philosophy is the highest music.","Dead on the inside, dead on the outside","There is nothing either good or bad, but thinking makes it so.","I'm the dude, so that's what you call me. That or, uh His Dudeness, or uh Duder, or El Duderino, if you're not into the whole brevity thing.","Laughter is poison to fear."]
 instance Arbitrary (Realistic Bio) where
   arbitrary = Realistic . Bio <$> genRealisticText
-  shrink (Realistic (Bio b)) = Realistic . Bio <$> shrink b
 
 -- | @since 0.2.0.0
 -- >>> sample' $ arbitrary @(Realistic Image)
@@ -129,28 +127,24 @@ instance Arbitrary (Realistic Image) where
 -- ["Baptism of Fire","Use of Weapons","Beyond the Mexique Bay","The Hydrogen Sonata","Naib","Padishah Emperor","Judge of the Change","Count","Baptism of Fire","No Longer at Ease","Harry Potter and the Goblet of Fire"]
 instance Arbitrary (Realistic Title) where
   arbitrary = Realistic . Title <$> genRealisticTitle
-  shrink (Realistic (Title tt)) = Realistic . Title <$> shrink tt
 
 -- | @since 0.2.0.0
 -- >>> sample' $ arbitrary @(Realistic Description)
 -- ["When you play a game of thrones you win or you die.","The world is grown so bad that wrens make prey where eagles dare not perch.","Portnoy finds joy in hoi polloi boy toy","Dad, Tom Cruise won't come out of the closet!","Portnoy finds joy in hoi polloi boy toy","When you play a game of thrones you win or you die.","Life is short butters, & thats why you have to do whatever you want all the time","I know there's no way I can convince you this is not one of their tricks, but I don't care, I am me. My name is Valerie, I don't think I'll live much longer and I wanted to tell someone about my life. This is the only autobiography I'll ever write, and god, I'm writing it on toilet paper. I was born in Nottingham in 1985, I don't remember much of those early years, but I do remember the rain. My grandmother owned a farm in Tuttlebrook, and she use to tell me that god was in the rain. I passed my 11th lesson into girl's grammar; it was at school that I met my first girlfriend, her name was Sara. It was her wrists. They were beautiful. I thought we would love each other forever. I remember our teacher telling us that is was an adolescent phase people outgrew. Sara did, I didn't. In 2002, I fell in love with a girl named Christina. That year I came out to my parents. I couldn't have done it without Chris holding my hand. My father wouldn't look at me, he told me to go and never come back. My mother said nothing. But I had only told them the truth, was that so selfish? Our integrity sells for so little, but it is all we really have. It is the very last inch of us, but within that inch, we are free. I'd always known what I wanted to do with my life, and in 2015 I starred in my first film, 'The Salt Flats'. It was the most important role of my life, not because of my career, but because that was how I met Ruth. The first time we kissed, I knew I never wanted to kiss any other lips but hers again. We moved to a small flat in London together. She grew Scarlet Carsons for me in our window box, and our place always smelled of roses. Those were there best years of my life. But America's war grew worse, and worse. And eventually came to London. After that there were no roses anymore. Not for anyone. I remember how the meaning of words began to change. How unfamiliar words like 'collateral' and 'rendition' became frightening. While things like Norse Fire and The Articles of Allegiance became powerful, I remember how different became dangerous. I still don't understand it, why they hate us so much. They took Ruth while she was out buying food. I've never cried so hard in my life. It wasn't long till they came for me. It seems strange that my life should end in such a terrible place, but for three years, I had roses, and apologized to no one. I shall die here. Every inch of me shall perish. Every inch, but one. An Inch, it is small and it is fragile, but it is the only thing the world worth having. We must never lose it or give it away. We must never let them take it from us. I hope that whoever you are, you escape this place. I hope that the world turns and that things get better. But what I hope most of all is that you understand what I mean when I tell you that even though I do not know you, and even though I may never meet you, laugh with you, cry with you, or kiss you. I love you. With all my heart, I love you. -Valerie","The secret to humor is surprise.","Dead on the inside, dead on the outside","Oh, not tonight Bishop... not tonight!"]
 instance Arbitrary (Realistic Description) where
   arbitrary = Realistic . Description <$> genRealisticText
-  shrink (Realistic (Description d)) = Realistic . Description <$> shrink d
 
 -- | @since 0.2.0.0
 -- >>> sample' $ arbitrary @(Realistic Body)
 -- ["A lion doesn't concern itself with the opinion of sheep.","A few vices are sufficient to darken many virtues.","See how she leans her cheek upon her hand. Oh, that I were a glove upon that hand that I might touch that cheek!","I wish I wasn't afraid all the time, but... I am.","Stop counting only those things you have lost! What is gone, is gone! So ask yourself this. What is there... that still remains to you?!","Hey, I know that guy. He's a nihilist. Karl Hungus.","Dead on the inside, dead on the outside","Dead on the inside, dead on the outside","Dead on the inside, dead on the outside","When you play a game of thrones you win or you die.","Your mother was worried sick and I was here drinking beer"]
 instance Arbitrary (Realistic Body) where
   arbitrary = Realistic . Body <$> genRealisticText
-  shrink (Realistic (Body b)) = Realistic . Body <$> shrink b
 
 -- | @since 0.2.0.0
 -- >>> sample' $ arbitrary @(Realistic Tag)
 -- ["Cloned","stygian","expansion play","mission-critical","fainted","tofu","crucifix","get a pulse on","penetrate the market","Function-based","small batch"]
 instance Arbitrary (Realistic Tag) where
   arbitrary = Realistic . Tag <$> genRealisticBuzzword
-  shrink (Realistic (Tag t)) = Realistic . Tag <$> shrink t
 
 instance Arbitrary (Realistic a) => Arbitrary (Realistic [a]) where
   arbitrary = Realistic <$> (getRealistic <<$>> listOf arbitrary)
