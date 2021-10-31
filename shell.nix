1b8f6d74d4c256b37706b63175a8a736e4ce82b8
fanshi1028 <jackychany321@gmail.com>
2021-10-31 15:25:55 +0800

    fix plan-sha256 for tool

--- shell.nix
+++ shell.nix
@@ -19,31 +19,31 @@ in project.shellFor {
     cabal = {
       inherit index-state checkMaterialization;
       version = "3.6.0.0";
-      plan-sha256 = "mZAX06zTy8Z9u7G2wdhi7XZHB7LcS0j+2AvKou5ohlg=";
+      plan-sha256 = "0n46d3pa5jhbv3z4hjywn83lfxpdcbcc3dmipdywdjykmk9ig44r";
       materialized = materializedDir + /cabal;
     };
     hlint = {
       inherit index-state checkMaterialization;
       version = "3.3.4";
-      plan-sha256 = "FBldDuwKPskZaFcr+7n1auXdoaEh5c7R1vHAx3V6zGs=";
+      plan-sha256 = "0sycg9swgh7isv8wxr91l6hxvrbaynwznaspd0cwjghaxh75s68l";
       materialized = materializedDir + /hlint;
     };
     haskell-language-server = {
       inherit index-state checkMaterialization;
       version = "1.4.0.0";
-      plan-sha256 = "gSxWYtUOf+orTraidRPJxqNGhtsgN/hjFhaHZuHHeg4=";
+      plan-sha256 = "03ksqzhnd1qn2rizhdr0vf34d8y6r49pb8mn9qmylzqfsmi5cb41";
       materialized = materializedDir + /haskell-language-server;
     };
     ormolu = {
       inherit index-state checkMaterialization;
       version = "0.3.0.1";
-      plan-sha256 = "VmCJfNcu5aNn45zQnklVPj4soK+Vr2FLWbtZMgjEz50=";
+      plan-sha256 = "17fgqh434ndvb55n3bwmmyh2qgiyam4rxl4wwdks7r9fsxy8jq2n";
       materialized = materializedDir + /ormolu;
     };
     ghcid = {
       inherit index-state checkMaterialization;
       version = "0.8.7";
-      plan-sha256 = "IopjzwLKTchf7SEwgzXQkOZVpFWNdm1XRxs9TK56nUU=";
+      plan-sha256 = "0icxgap4qg8v8xbnsxldanj5brlhs0sq6c11xmgwhkfa0b7n72i2";
       materialized = materializedDir + /ghcid;
     };
     # use cabal-docspec instead of doctest(which I failed to set it up right), yet it seesm that cabal-docspec is not on hackage yet
@@ -52,13 +52,13 @@ in project.shellFor {
     cabal-fmt = {
       inherit index-state checkMaterialization;
       version = "0.1.5.1";
-      plan-sha256 = "uFjbI0JaWBvJ7TpsyhdQ+dUO+8jO5tnxZgCqpJPciAM=";
+      plan-sha256 = "00w8vj9s9ah0cvqxkrnfr3xhxmgra0bwlv1sxp4inn2s88ixnn5q";
       materialized = materializedDir + /cabal-fmt;
     };
     stan = {
       inherit index-state checkMaterialization;
       version = "0.0.1.0";
-      plan-sha256 = "ZVtScPrqYe8/wNGG9wMEnHsPkJ7x5QSTRDeXkOoLPKI=";
+      plan-sha256 = "18iw1gm915rp8j9h9rgiks80yyww0h1zg1niq0zyyqgaz9q54nv5";
       materialized = materializedDir + /stan;
     };
     # hoogle = {
