659f93c4a5426719adb368821026c821ddc37c99
fanshi1028 <jackychany321@gmail.com>
2021-11-14 19:02:55 +0800

    add streamly dependency

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
-with (import ./nix/pkgs.nix).haskell-nix;
-project {
+with import ./nix/pkgs.nix;
+haskell-nix.project {
   # 'cleanGit' cleans a source directory based on the files known by git
-  src = haskellLib.cleanGit {
+  src = haskell-nix.haskellLib.cleanGit {
     name = "realworld-haskell";
     src = ./.;
   };
@@ -13,6 +13,12 @@ project {
   # plan-sha256 = "RiN1455qlA4rp2yO6wJp9FCVzAOR20KzfmNMM3fPXTQ=";
 
   modules = [{
+    # https://github.com/composewell/streamly/issues/1132
+    # https://github.com/NixOS/cabal2nix/issues/470
+    # https://github.com/input-output-hk/haskell.nix/issues/1164
+    packages.streamly.components.library.libs =
+      lib.optionals (builtins.currentSystem == "x86_64-darwin")
+      [ darwin.apple_sdk.frameworks.Cocoa ];
     packages.realworld-haskell.components.exes.realworld-haskell = {
       dontStrip = false;
       ghcOptions = [ "-O2" ];
