72f2845847d9eb5ea3e3e4bfe4379047120ee7a7
fanshi1028 <jackychany321@gmail.com>
2021-11-28 22:40:13 +0800

    refactor: simplify Relation.toOne.InMem effect

--- backend/src/effect/Relation/ToOne/InMem.hs
+++ backend/src/effect/Relation/ToOne/InMem.hs
@@ -12,47 +12,24 @@
 -- Carrier to run in memory
 --
 -- @since 0.1.0.0
-module Relation.ToOne.InMem (ExistAction (..), C (..), run) where
+module Relation.ToOne.InMem (C (..), run) where
 
 import Control.Algebra (Algebra (alg), type (:+:) (L, R))
 import Control.Effect.Lift (Lift, sendM)
 import Control.Effect.Sum (Member)
-import qualified Focus as FC (Change (Leave, Remove, Set), unitCases)
+import qualified Focus (update)
 import GHC.TypeLits (Symbol)
 import Relation.ToOne (E (GetRelated, Relate, Unrelate))
-import qualified StmContainers.Map as STM (Map, focus, lookup)
+import qualified StmContainers.Map as STM (Map, focus, insert, lookup)
 
--- | @since 0.1.0.0
--- Action to token when exists on relation insertion
-data ExistAction = UpdateIfExist | IgnoreIfExist
-
--- | @since 0.1.0.0
-newtype C (r1 :: Type) (r :: Symbol) (r2 :: Type) (ex :: ExistAction) (m :: Type -> Type) a = C
+-- | @since 0.3.0.0
+newtype C (r1 :: Type) (r :: Symbol) (r2 :: Type) (m :: Type -> Type) a = C
   { -- | @since 0.1.0.0
     run' :: ReaderT (STM.Map r1 r2) m a
   }
   deriving (Functor, Applicative, Monad, MonadReader (STM.Map r1 r2))
 
--- | @since 0.1.0.0
-relateFocus :: (Eq key, Hashable key) => ExistAction -> key -> value -> STM.Map key value -> STM ()
-relateFocus existAction k v =
-  STM.focus
-    ( FC.unitCases
-        (FC.Set v)
-        $ const $
-          case existAction of
-            UpdateIfExist -> FC.Set v
-            IgnoreIfExist -> FC.Leave
-    )
-    k
-{-# INLINE relateFocus #-}
-
--- | @since 0.1.0.0
-unrelateFocus :: (Hashable key, Eq key, Eq value) => key -> value -> STM.Map key value -> STM ()
-unrelateFocus k v = STM.focus (FC.unitCases FC.Leave (\e -> if v == e then FC.Remove else FC.Leave)) k
-{-# INLINE unrelateFocus #-}
-
--- | @since 0.2.0.0
+-- | @since 0.3.0.0
 instance
   ( Algebra sig m,
     Member (Lift STM) sig,
@@ -60,42 +37,23 @@ instance
     Eq r2,
     Hashable r1
   ) =>
-  Algebra (E r1 r r2 :+: sig) (C r1 r r2 'IgnoreIfExist m)
+  Algebra (E r1 r r2 :+: sig) (C r1 r r2 m)
   where
   alg _ (L action) ctx =
     ask
       >>= fmap (<$ ctx) . sendM @STM . case action of
-        Relate r1 r2 -> relateFocus IgnoreIfExist r1 r2
-        Unrelate r1 r2 -> unrelateFocus r1 r2
-        GetRelated r1 -> STM.lookup r1
-  alg hdl (R other) ctx = C $ alg (run' . hdl) (R other) ctx
-  {-# INLINE alg #-}
-
--- | @since 0.2.0.0
-instance
-  ( Algebra sig m,
-    Member (Lift STM) sig,
-    Eq r1,
-    Eq r2,
-    Hashable r1
-  ) =>
-  Algebra (E r1 r r2 :+: sig) (C r1 r r2 'UpdateIfExist m)
-  where
-  alg _ (L action) ctx =
-    ask
-      >>= fmap (<$ ctx) . sendM @STM . case action of
-        Relate r1 r2 -> relateFocus UpdateIfExist r1 r2
-        Unrelate r1 r2 -> unrelateFocus r1 r2
+        Relate r1 r2 -> STM.insert r2 r1
+        Unrelate r1 r2 -> STM.focus (Focus.update (\v -> if v == r2 then Nothing else Just v)) r1
         GetRelated r1 -> STM.lookup r1
   alg hdl (R other) ctx = C $ alg (run' . hdl) (R other) ctx
   {-# INLINE alg #-}
 
 -- | @since 0.1.0.0
 run ::
-  forall r1 r r2 ex a m.
+  forall r1 r r2 a m.
   -- | the in-memory db
   STM.Map r1 r2 ->
   -- | the effect stack
-  C r1 r r2 ex m a ->
+  C r1 r r2 m a ->
   m a
 run db = run' >>> usingReaderT db
