8acb10e847cd384bef165fdf59a5303cc9166a10
fanshi <jackychany321@gmail.com>
2022-01-26 14:40:09 +0800

    init frontend/app/Main.hs

--- frontend/app/Main.hs
+++ frontend/app/Main.hs
@@ -1,7 +1,29 @@
--- |
+{-# LANGUAGE RecursiveDo #-}
 
+-- | @since 0.4.0.0
 module Main where
 
+import Graphics.Vty (Key (KChar, KEsc), Modifier (MCtrl))
+import InVty.Scene.LoggedIn (loggedInPages)
+import InVty.Scene.LoggedOut (loggedOutPages)
+import Network.HTTP.Client (newManager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Reflex (never, switchDyn)
+import Reflex.Vty (initManager_, keyCombos, mainWidget)
+import Reflex.Workflow (Workflow (Workflow), workflow)
+import Servant.Client (BaseUrl (BaseUrl), Scheme (Https), mkClientEnv)
+
+-- | @since 0.4.0.0
 main :: IO ()
 main = do
-  putText "Surprise Motherfucker!"
+  clientEnv <- mkClientEnv <$> newManager tlsManagerSettings ?? BaseUrl Https "api.realworld.io" 443 ""
+  mainWidget $
+    initManager_ $ mdo
+      let loggedInWF login = Workflow $ do
+            e <- loggedInPages clientEnv login
+            pure (never, loggedOutWF <$ e)
+          loggedOutWF = Workflow $ do
+            e <- loggedOutPages clientEnv
+            pure (never, loggedInWF <$> e)
+      void $ switchDyn <$> workflow loggedOutWF
+      (() <$) <$> keyCombos (fromList [(KChar 'c', [MCtrl]), (KEsc, [])])
