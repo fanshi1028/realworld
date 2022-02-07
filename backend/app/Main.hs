1c954e8fb938e52fbfd524cfe4fd2a9a7b97b60c
fanshi <jackychany321@gmail.com>
2022-02-07 19:49:23 +0800

    refactor cabal file: use more internal lib

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -1,11 +1,24 @@
+{-# LANGUAGE CPP #-}
+
 module Main where
 
+#if backendinmem
 import InMem.App (newApp)
+#else if backendrel8
+import InRel8.App (newApp)
+#endif
+
 import Network.Wai.Handler.Warp (run)
 
 main :: IO ()
 main = do
-  putStrLn $ "server running at port: " <> show port
-  newApp >>= run port
+  putStrLn $
+    "server running at port: " <> show port
+#if backendinmem
+  InMem.App.newApp
+#else if backendrel8
+  InRel8.App.newApp ""
+#endif
+      >>= run port
   where
     port = 8080
